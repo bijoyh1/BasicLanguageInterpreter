59186556f18f02ceec2d8c3be4e31019af7bd9a3
Gordon Barnes <32275570+gbarnes888@users.noreply.github.com>
2021-03-29 15:34:53 -0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -83,6 +83,7 @@ variables = {
     'X': 20001,
     'Y': 20002
 }
+operators = {'+', '-', '*', '/', '^'}
 f = open("code 1 Final.txt",'r')
 g = open("code 1 Parsed.txt",'w')
 
@@ -109,52 +110,71 @@ def rem(x):
 def if_statement(x):
     y =x[0:2] + x[x.index(keytable.get(')'))+1:]
     t =x[0:2] + x[x.index(keytable.get('('))+1:x.index(keytable.get(')'))]
-    return " -> <boolean_expression>" + str(boolean_statement(t)) + str(statement_type(y))
+    return " -><if_statement> -> <boolean_expression>" + str(boolean_statement(t)) + str(statement_type(y))
 
 def goto(x):
     y= x[0:2] + x[3:]
     return " -> <goto_statement>" + str(statement_type(y))
 
+def assignment_statement(x):
+    y = x[0:2] + x[x.index(keytable.get('='))+1:]
+    t = x[0:3]
+    return " -> <assignment_statement> -> <Literal_variable> <eq_operator> <arithmetic_expression>\n" + statement_type(t) + "<eq_operator> -> =\n" \
+            + str(statement_type(y))
+
+
 def arithmetic_statement(x):
-    return ""
+    operator = ''
+    index = 0
+    for op in operators:
+        if keytable.get(op) in x:
+            operator = op
+            index = x[x.index(keytable.get(op))]
+            break
+
+    y = x[0:x.index(keytable.get(operator))]
+    z = x[0:2] + x[x.index(keytable.get(operator)) + 1:]
+    return "<arithmetic_statement> -> <arithmetic_statement> <operator> <arithmetic_statement>\n" + str(statement_type(y)) \
+           + "<operator> -> " + operator + "\n"\
+           + str(statement_type(z))
 
 def literal_variable(x):
     return "-> <literal_variable>\n<literal_variable> -> " + get_key_variables(int(x[2])) + "\n"
 
 def literal_integer(x):
-    return "-> <literal_integer>\n<literal_integer> -> " + x[2]+ "\n"
+    return "-> <literal_integer>\n<literal_integer> -> " + x[2] + "\n"
 
 def boolean_statement(x):
     return statement_type(x[0:3]) + " -> <boolean_operator>\n<boolean_operator> -> " + get_key_keytable(x[3]) + "\n" + statement_type(x[0:2] + x[4:])
+
+def end(x):
+    return "-> <Command>\n<Command> -> END"
 #Expression Types
 
 
 
 #Finds type of statement
 def statement_type(x):
-    if x[2] == keytable.get('LET'):
-        print("LET statment")
+    if x[2] == keytable.get('END'):
+        return end(x)
+    elif x[2] == keytable.get('LET'):
         return let(x)
     elif x[2] == keytable.get('REM'):
-        print("REM statment")
         return rem(x)
     elif x[2] == keytable.get('PRINT'):
-        print("PRINT statment")
         return print_out(x)
     elif x[2] == keytable.get('IF'):
-        print("IF statment")
         return str(if_statement(x))
     elif x[2] == keytable.get('GOTO'):
-        print("GOTO statment")
         return goto(x)
-    # elif ADD CODE HERE:
-    #     print("arithmetic statement")
-    #     return arithmetic_statement(x)
+    elif int(x[2]) in variables.values() and len(x) >= 4 and x[3] == keytable.get('='):
+        return assignment_statement(x)
+    elif keytable.get('+') in x or keytable.get('-') in x or keytable.get('/') in x or keytable.get(
+            '*') in x or keytable.get('^') in x:
+        return arithmetic_statement(x)
     elif int(x[2]) in variables.values():
-        print("literal Variable")
         return literal_variable(x)
-    elif x[2] not in keytable.keys() or x[2] not in variables.values():
-        print("literal Integer")
+    elif x[2] not in keytable.values() or x[2] not in variables.values():
         return literal_integer(x)
     return ""
 
@@ -167,3 +187,4 @@ def create_grammar(x):
 for x in f:
     x = x.split(" ")
     create_grammar(x)
+    print()
